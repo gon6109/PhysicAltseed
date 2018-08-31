c445b0fb3ff0d026bc3eee60745196ae9951dfaa
gon <hutabasuzukiryu@live.jp>
2018-08-31 15:09:47 +0900

    多角形の対応

---
+++ PhysicAltseed/PhysicalPolygonShape.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using Box2DNet.Collision;
+using Box2DNet.Common;
+using Box2DNet.Dynamics;
+using System.Linq;
+namespace PhysicAltseed
+{
+    public class PhysicalPolygonShape : asd.PolygonShape, PhysicalShape
+    {
+        BodyDef b2BodyDef;
+        List<PolygonDef> b2PolygonDefs;
+        Body b2Body;
+        PhysicalShapeType physicalShapeType;
+        PhysicalWorld refWorld;
+        private List<asd.Vector2DF> vertexes;
+
+        public Body B2Body
+        {
+            get
+            {
+                return b2Body;
+            }
+        }
+
+        private float angle;
+        /// <summary>
+        /// 角度
+        /// </summary>
+        public float Angle
+        {
+            get
+            {
+                return angle;
+            }
+            set
+            {
+                angle = value;
+                Reset();
+            }
+        }
+
+        private asd.Vector2DF centerPosition;
+        /// <summary>
+        /// グローバル座標
+        /// </summary>
+        public asd.Vector2DF CenterPosition
+        {
+            get
+            {
+                return centerPosition;
+            }
+            set
+            {
+                centerPosition = value;
+                Reset();
+            }
+        }
+
+        float density;
+        /// <summary>
+        /// 密度
+        /// </summary>
+        public float Density
+        {
+            get
+            {
+                return density;
+            }
+            set
+            {
+                density = value;
+                Reset();
+            }
+        }
+
+        float restitution;
+        /// <summary>
+        /// 反発係数
+        /// </summary>
+        public float Restitution
+        {
+            get
+            {
+                return restitution;
+            }
+            set
+            {
+                restitution = value;
+                Reset();
+            }
+        }
+
+        float friction;
+        /// <summary>
+        /// 摩擦係数
+        /// </summary>
+        public float Friction
+        {
+            get
+            {
+                return friction;
+            }
+            set
+            {
+                friction = value;
+                Reset();
+            }
+        }
+
+        /// <summary>
+        /// 速度
+        /// </summary>
+        public asd.Vector2DF Velocity
+        {
+            get
+            {
+                if (!IsActive) return new asd.Vector2DF();
+                return PhysicalConvert.ToAsdVector(b2Body.GetLinearVelocity());
+            }
+            set
+            {
+                if (!IsActive) return;
+                if (physicalShapeType == PhysicalShapeType.Kinematic) b2Body.SetPosition(PhysicalConvert.Tob2Vector(value * refWorld.TimeStep) + b2Body.GetPosition());
+                else b2Body.SetLinearVelocity(PhysicalConvert.Tob2Vector(value));
+            }
+        }
+
+        /// <summary>
+        /// 角速度
+        /// </summary>
+        public float AngularVelocity
+        {
+            get
+            {
+                if (!IsActive) return 0;
+                return b2Body.GetAngularVelocity() * 180.0f / 3.14f;
+            }
+            set
+            {
+                if (!IsActive) return;
+                if (physicalShapeType == PhysicalShapeType.Kinematic) b2Body.SetAngle(value / 180.0f * 3.14f + b2Body.GetAngle());
+                else b2Body.SetAngularVelocity(value / 180.0f * 3.14f);
+            }
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        /// <param name="shapeType">物理形状タイプ</param>
+        /// <param name="world">登録するワールド</param>
+        public PhysicalPolygonShape(PhysicalShapeType shapeType, PhysicalWorld world)
+        {
+            density = 1.0f;
+            restitution = 0.3f;
+            angle = 0.0f;
+            b2BodyDef = new BodyDef();
+            b2PolygonDefs = new List<PolygonDef>();
+            vertexes = new List<asd.Vector2DF>();
+
+            refWorld = world;
+            physicalShapeType = shapeType;
+            b2Body = refWorld.B2World.CreateBody(b2BodyDef);
+            foreach (var item in b2PolygonDefs) b2Body.CreateFixture(item);
+            if (physicalShapeType == PhysicalShapeType.Dynamic) b2Body.SetMassFromShapes();
+            world.Add(this);
+        }
+
+        /// <summary>
+        /// 削除する
+        /// </summary>
+        public void Destroy()
+        {
+            refWorld.Destroy(this);
+            Dispose();
+        }
+
+        /// <summary>
+        /// 多角形を構成する頂点を追加する
+        /// </summary>
+        /// <param name="vertex">追加する頂点</param>
+        public new void AddVertex(asd.Vector2DF vertex)
+        {
+            if (vertexes.FindAll(obj => obj == vertex).Count != 0) return;
+            vertexes.Add(vertex);
+            base.AddVertex(vertex);
+            Reset();
+        }
+
+        /// <summary>
+        /// 多角形を構成する頂点を全て削除する
+        /// </summary>
+        public new void ClearVertexes()
+        {
+            vertexes.Clear();
+            base.ClearVertexes();
+            Reset();
+        }
+
+        public void Reset()
+        {
+            if (vertexes.Count < 3) return;
+
+            if (b2Body != null) refWorld.B2World.DestroyBody(b2Body);
+
+            b2BodyDef = new BodyDef();
+
+            b2BodyDef.Angle = Angle / 180.0f * 3.14f;
+            b2BodyDef.Position = PhysicalConvert.Tob2Vector(CenterPosition);
+
+            b2PolygonDefs = DivideToTriangles(vertexes);
+            b2Body = refWorld.B2World.CreateBody(b2BodyDef);
+
+            foreach (var item in b2PolygonDefs)
+            {
+                b2Body.CreateFixture(item);
+            }
+
+            if (physicalShapeType == PhysicalShapeType.Dynamic) b2Body.SetMassFromShapes();
+        }
+
+        List<PolygonDef> DivideToTriangles(List<asd.Vector2DF> argVertexes)
+        {
+            if (argVertexes.Count < 3) return null;
+
+            List<PolygonDef> result = new List<PolygonDef>();
+            if (argVertexes.Count == 3)
+            {
+                result.Add(CreatePolygonDef(argVertexes[0], argVertexes[1], argVertexes[2]));
+                return result;
+            }
+
+            asd.Vector2DF root = new asd.Vector2DF();
+            foreach (var item in argVertexes)
+            {
+                if (root.Length < item.Length) root = item;
+            }
+
+            asd.Vector2DF next1, next2;
+            next1 = argVertexes.IndexOf(root) != argVertexes.Count - 1 ? argVertexes[argVertexes.IndexOf(root) + 1] : argVertexes[0];
+            next2 = argVertexes.IndexOf(root) != 0 ? argVertexes[argVertexes.IndexOf(root) - 1] : argVertexes[argVertexes.Count - 1];
+
+            float cross = asd.Vector2DF.Cross(next1 - root, next2 - root);
+            while (true)
+            {
+                bool isDivideble = true;
+                foreach (var item in argVertexes)
+                {
+                    if (IsContainAtTriangle(root, next1, next2, item)) isDivideble = false;
+                }
+
+                if (!isDivideble)
+                {
+                    do
+                    {
+                        root = argVertexes.IndexOf(root) != argVertexes.Count - 1 ? argVertexes[argVertexes.IndexOf(root) + 1] : argVertexes[0];
+                        next1 = argVertexes.IndexOf(next1) != argVertexes.Count - 1 ? argVertexes[argVertexes.IndexOf(next1) + 1] : argVertexes[0];
+                        next2 = argVertexes.IndexOf(next2) != argVertexes.Count - 1 ? argVertexes[argVertexes.IndexOf(next2) + 1] : argVertexes[0];
+                    } while (System.Math.Sign(cross) != System.Math.Sign(asd.Vector2DF.Cross(next1 - root, next2 - root)));
+                }
+                else break;
+            }
+
+            result.Add(CreatePolygonDef(root, next1, next2));
+            List<asd.Vector2DF> remain = new List<asd.Vector2DF>(argVertexes);
+            remain.Remove(root);
+            result.AddRange(DivideToTriangles(remain));
+            return result;
+        }
+
+        bool IsContainAtTriangle(asd.Vector2DF vertex1, asd.Vector2DF vertex2, asd.Vector2DF vertex3, asd.Vector2DF vector)
+        {
+            float c1 = asd.Vector2DF.Cross(vertex2 - vertex1, vector - vertex2);
+            float c2 = asd.Vector2DF.Cross(vertex3 - vertex2, vector - vertex3);
+            float c3 = asd.Vector2DF.Cross(vertex1 - vertex3, vector - vertex1);
+
+            if ((c1 > 0 && c2 > 0 && c3 > 0) || (c1 < 0 && c2 < 0 && c3 < 0)) return true;
+
+            return false;
+        }
+
+        PolygonDef CreatePolygonDef(asd.Vector2DF vertex1, asd.Vector2DF vertex2, asd.Vector2DF vertex3)
+        {
+            PolygonDef b2PolygonDef = new PolygonDef();
+            var sortedVertexes = new List<asd.Vector2DF>();
+            sortedVertexes.Add(vertex1);
+            sortedVertexes.Add(vertex2);
+            sortedVertexes.Add(vertex3);
+            sortedVertexes.Sort((a, b) => a.Degree.CompareTo(b.Degree));
+            b2PolygonDef.VertexCount = 3;
+            for (int i = 0; i < 3; i++)
+            {
+                b2PolygonDef.Vertices[i] = PhysicalConvert.Tob2Vector(sortedVertexes[i]);
+            }
+
+            b2PolygonDef.Density = Density;
+            b2PolygonDef.Restitution = Restitution;
+            b2PolygonDef.Friction = Friction;
+            return b2PolygonDef;
+        }
+
+        /// <summary>
+        /// 力を加える
+        /// </summary>
+        /// <param name="vector">力を加える方向</param>
+        /// <param name="position">力を加えるローカル位置</param>
+        public void SetForce(asd.Vector2DF vector, asd.Vector2DF position)
+        {
+            if (!IsActive) return;
+            b2Body.ApplyForce(PhysicalConvert.Tob2Vector(vector), PhysicalConvert.Tob2Vector(CenterPosition + position));
+        }
+
+        /// <summary>
+        /// 衝撃を加える
+        /// </summary>
+        /// <param name="vector">衝撃を加える方向</param>
+        /// <param name="position">衝撃を加えるローカル位置</param>
+        public void SetImpulse(asd.Vector2DF vector, asd.Vector2DF position)
+        {
+            if (!IsActive) return;
+            b2Body.ApplyImpulse(PhysicalConvert.Tob2Vector(vector), PhysicalConvert.Tob2Vector(CenterPosition + position));
+        }
+
+        public void SyncB2body()
+        {
+            if (!IsActive) return;
+            centerPosition = PhysicalConvert.ToAsdVector(b2Body.GetPosition());
+            angle = b2Body.GetAngle() * 180.0f / 3.14f;
+            base.ClearVertexes();
+            foreach (var item in vertexes)
+            {
+                asd.Vector2DF temp = item;
+                temp.Degree += Angle;
+                base.AddVertex(CenterPosition + temp);
+            }
+        }
+
+        /// <summary>
+        /// 衝突判定
+        /// </summary>
+        /// <param name="shape">衝突判定対象</param>
+        public bool GetIsCollidedWith(PhysicalShape shape)
+        {
+            if (!IsActive) return false;
+            List<asd.Vector2DF> points;
+            return refWorld.GetIsCollided(this, shape, out points);
+        }
+
+        /// <summary>
+        /// 衝突判定
+        /// </summary>
+        /// <param name="shape">衝突判定対象</param>
+        /// <param name="points">衝突点</param>
+        public bool GetIsCollidedWith(PhysicalShape shape, out List<asd.Vector2DF> points)
+        {
+            if (!IsActive)
+            {
+                points = new List<asd.Vector2DF>();
+                return false;
+            }
+            return refWorld.GetIsCollided(this, shape, out points);
+        }
+
+        /// <summary>
+        /// 物理シミュレーションをするか否か
+        /// </summary>
+        public bool IsActive
+        {
+            get
+            {
+                return b2Body != null;
+            }
+            set
+            {
+                if (value)
+                {
+                    if (IsActive == false) Reset();
+                }
+                else
+                {
+                    if (IsActive == true) b2Body.GetWorld().DestroyBody(b2Body);
+                    b2Body = null;
+                }
+            }
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -8,16 +8,16 @@ namespace Test
         {
             asd.Engine.Initialize("Test", 640, 480, new asd.EngineOption());
             PhysicAltseed.PhysicalConvert.PxPerMetreRate = 50;
-            PhysicAltseed.PhysicalWorld world = new PhysicAltseed.PhysicalWorld(new asd.RectF(new asd.Vector2DF(), asd.Engine.WindowSize.To2DF()), new asd.Vector2DF(0, 250.0f));
+            PhysicAltseed.PhysicalWorld world = new PhysicAltseed.PhysicalWorld(new asd.RectF(-200,-200,1040,880), new asd.Vector2DF(0, 250.0f));
 
-            asd.GeometryObject2D geometryObject = new asd.GeometryObject2D();
-            PhysicAltseed.PhysicalRectangleShape shape = new PhysicAltseed.PhysicalRectangleShape(PhysicAltseed.PhysicalShapeType.Dynamic, world);
-            shape.DrawingArea = new asd.RectF(300, 50, 25, 25);
-            shape.Friction = 0.6f;
-            shape.Restitution = 0.8f;
-            shape.Density = 1.0f;
-            geometryObject.Shape = shape;
-            asd.Engine.AddObject2D(geometryObject);
+            //asd.GeometryObject2D geometryObject = new asd.GeometryObject2D();
+            //PhysicAltseed.PhysicalRectangleShape shape = new PhysicAltseed.PhysicalRectangleShape(PhysicAltseed.PhysicalShapeType.Dynamic, world);
+            //shape.DrawingArea = new asd.RectF(300, 50, 25, 25);
+            //shape.Friction = 0.6f;
+            //shape.Restitution = 0.8f;
+            //shape.Density = 1.0f;
+            //geometryObject.Shape = shape;
+            //asd.Engine.AddObject2D(geometryObject);
 
             asd.GeometryObject2D groundObject = new asd.GeometryObject2D();
             PhysicAltseed.PhysicalRectangleShape ground = new PhysicAltseed.PhysicalRectangleShape(PhysicAltseed.PhysicalShapeType.Static, world);
@@ -27,47 +27,40 @@ namespace Test
             groundObject.Shape = ground;
             asd.Engine.AddObject2D(groundObject);
 
-            // 動的フォントを生成する。
-            var font = asd.Engine.Graphics.CreateDynamicFont("", 32, new asd.Color(255, 255, 255, 255), 1, new asd.Color(255, 255, 255, 255));
-
-            // FPSを表示するためのオブジェクトを生成する。
-            var obj = new asd.TextObject2D();
-            obj.Font = font;
-
-            // オブジェクトをエンジンに追加する。
-            asd.Engine.AddObject2D(obj);
-            int c = 0;
+            asd.Engine.ProfilerIsVisible = true;
 
             while (asd.Engine.DoEvents())
             {
-                if (asd.Engine.Keyboard.GetKeyState(asd.Keys.Left) == asd.KeyState.Hold) shape.SetForce(new asd.Vector2DF(25, 0), shape.CenterPosition);//Velocity = new asd.Vector2DF(-25, shape.Velocity.Y);
                 world.Update();
                 asd.Engine.Update();
-
-                if (c % 200 == 0) shape.IsActive = true;
-                else if (c % 200 == 100) shape.IsActive = false;
 
                 if (asd.Engine.Mouse.LeftButton.ButtonState == asd.MouseButtonState.Push)
                 {
                     asd.GeometryObject2D geometryObject2 = new asd.GeometryObject2D();
-                    PhysicAltseed.PhysicalRectangleShape shape2 = new PhysicAltseed.PhysicalRectangleShape(PhysicAltseed.PhysicalShapeType.Dynamic, world);
-                    shape2.DrawingArea = new asd.RectF(asd.Engine.Mouse.Position, new asd.Vector2DF(25, 25));
+                    PhysicAltseed.PhysicalPolygonShape shape2 = new PhysicAltseed.PhysicalPolygonShape(PhysicAltseed.PhysicalShapeType.Dynamic, world);
+                    shape2.CenterPosition = asd.Engine.Mouse.Position;
+                    for (int i = 0; i < 10; i++)
+                    {
+                        asd.Vector2DF vec = new asd.Vector2DF(1, 0);
+                        vec.Degree = i * 36;
+
+                        if (i % 2 == 0)
+                        {
+                            vec.Length = 50;
+                        }
+                        else
+                        {
+                            vec.Length = 22;
+                        }
+
+                        shape2.AddVertex(vec);
+                    }
                     shape2.Friction = 0.6f;
                     shape2.Restitution = 0.8f;
                     shape2.Density = 1.0f;
                     geometryObject2.Shape = shape2;
                     asd.Engine.AddObject2D(geometryObject2);
                 }
-
-                // 現在のFPSを取得する。
-                float fps = asd.Engine.CurrentFPS;
-
-                // 表示する文字列を生成する。
-                var str = "FPS : " + fps;
-
-                // 文字列をオブジェクトに設定する。
-                obj.Text = str;
-                c++;
             }
 
             asd.Engine.Terminate();
